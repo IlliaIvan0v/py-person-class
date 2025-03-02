230061e20d8582f40306658b848c059e6d2e1f02
Illia Ivanov <Illuha.Iva@gmail.com>
2025-03-02 12:49:16 +0100

    Re-request review

--- app/main.py
+++ app/main.py
@@ -1,24 +1,20 @@
 class Person:
-
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
+        Person.people[name] = self  # Store instance by name
 
 
 def create_person_list(people: list) -> list:
-    person_instances = {}
-
-    for person in people:
-        person_instances[person["name"]] = Person(person["name"],
-                                                  person["age"])
+    person_instances = {p["name"]: Person(p["name"], p["age"]) for p in people}
 
     for person in people:
         person_instance = person_instances[person["name"]]
-        if "wife" in person and person["wife"]:
-            person_instance.wife = person_instances[person["wife"]]
-        if "husband" in person and person["husband"]:
-            person_instance.husband = person_instances[person["husband"]]
+        if person.get("wife"):
+            setattr(person_instance, "wife", person_instances[person["wife"]])
+        if person.get("husband"):
+            setattr(person_instance, "husband", person_instances[person["husband"]])
+
     return list(person_instances.values())
